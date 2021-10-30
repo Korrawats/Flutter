7b66bd53a50f84308d44b29a601698ab1732c94e
Korrawat <blackmoneys2542@gmail.com>
2021-10-31 01:38:21 +0700

    final_test

---
+++ pages/food/food_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:hello/models/food_item.dart';
+import 'package:hello/services/api.dart';
+
+import 'food_list_page.dart';
+
+
+class FoodPage extends StatefulWidget {
+  const FoodPage({Key? key}) : super(key: key);
+
+  @override
+  _FoodPageState createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
+  var _selectedBottomNavIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Your Order',
+          ),
+        ],
+        currentIndex: _selectedBottomNavIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedBottomNavIndex = index;
+          });
+        },
+      ),
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: _fetchFoods,
+        child: Icon(Icons.add),
+      ),*/
+      body: _selectedBottomNavIndex == 0
+          ? FoodListPage()
+          : Container(
+              child: Center(
+                child: Text('YOUR ORDER',
+                    style: Theme.of(context).textTheme.headline1),
+              ),
+            ),
+    );
+  }
+
+  _fetchFoods() async {
+    try {
+      var list = (await Api().fetch('foods')) as List<dynamic>;
+      var foodList = list.map((item) => FoodItem.fromJson(item)).toList();
+      print('Number of foods: ${foodList.length}');
+    } catch (e) {
+      var msg = 'ERROR: $e';
+      print(msg);
+    }
+  }
+}
